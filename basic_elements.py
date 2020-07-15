1d72ec63297800087574cc74b4035de0c56fb79e
juanjo-lg <juanjo_lg@hotmail.es>
2020-07-15 18:30:05 +0200

    first commit

---
+++ basic_elements.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wen Jul 15 12:51:00 2020
+
+@author: Juanjo_LG
+"""
+
+"""Elementos Básicos para aplicación de Topografía."""
+
+import math
+import numpy as np
+
+#Clase punto.
+class Point:
+
+    """
+    Se inicializa pasando las coordenadas X, Y, Z del punto para crear alguna
+    matriz con el vector de dicho punto.
+    También puede pasarse un código de punto o cualquier otro atributo.
+    """
+    #Constructor del punto.
+    def __init__(self, x, y, z=0, **kwargs):
+        self.coord = np.array([x, y, z])
+        if "n" in kwargs:
+            self.num = kwargs["n"]
+        if "cod" in kwargs:
+            self.cod = kwargs["cod"]
+        else:
+            self.cod = "Por defecto"
+    #Cálculo del módulo (distancia) entre dos vectores de dos puntos.
+    def distance(self, x=0, y=0, z=0):
+        return np.linalg.norm(self.coord-np.array([x,y,z]))
+
+"""Pruebas"""
+print("PRUEBAS DE FUNCIONAMIENTO")
+p1 = Point(2, 2, 0)
+p2 = Point(1, 1, 0)
+print("Coordenadas de Punto 1: %s" % (p1.coord))
+print(p1.distance(x=1,y=1,z=0))
+print(np.linalg.norm(p1.coord-p2.coord))
