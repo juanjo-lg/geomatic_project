bc219018dd081d86c069682e5dd3c2e0565f9d37
juanjo-lg <juanjo_lg@hotmail.es>
2020-07-16 13:35:11 +0200

    Update azimutç

--- basic_elements.py
+++ basic_elements.py
@@ -12,7 +12,6 @@ import numpy as np
 
 #Clase punto.
 class Point:
-
     """
     Se inicializa pasando las coordenadas X, Y, Z del punto para crear alguna
     matriz con el vector de dicho punto.
@@ -23,13 +22,21 @@ class Point:
         self.coord = np.array([x, y, z])
         if "n" in kwargs:
             self.num = kwargs["n"]
+        #En caso de no tener código, se le asigna uno por defecto.
         if "cod" in kwargs:
             self.cod = kwargs["cod"]
         else:
             self.cod = "Por defecto"
     #Cálculo del módulo (distancia) entre dos vectores de dos puntos.
     def distance(self, x=0, y=0, z=0):
+        """Devuelve el módulo del vector o la distancia entre dos puntos en
+        caso de que se añadan coordenadas a los parámetros del método"""
         return np.linalg.norm(self.coord-np.array([x,y,z]))
+    #Cálculo de azimut de un punto o entre dos puntos.
+    def azimut(self, x=0, y=0, z=0):
+        azim = (math.atan2(x-self.coord[0],y-self.coord[1]))*(200/math.pi)
+        return azim
+        #Hay que revisarlo!!!!!
 
 """Pruebas"""
 print("PRUEBAS DE FUNCIONAMIENTO")
@@ -38,3 +45,4 @@ p2 = Point(1, 1, 0)
 print("Coordenadas de Punto 1: %s" % (p1.coord))
 print(p1.distance(x=1,y=1,z=0))
 print(np.linalg.norm(p1.coord-p2.coord))
+print("azimut del punto 1: %s" % (p1.azimut(0,0,0)))
