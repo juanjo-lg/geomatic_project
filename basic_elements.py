077002884762f1f19101bab86d70aa8718fb38bf
juanjo-lg <juanjo_lg@hotmail.es>
2020-07-17 13:08:25 +0200

    Update basic_elements

--- basic_elements.py
+++ basic_elements.py
@@ -27,30 +27,45 @@ class Point:
             self.cod = kwargs["cod"]
         else:
             self.cod = "Por defecto"
+        #Mensaje de creación de cada instancia.
+        print("Creación de punto con coordenadas: %s" % (self.coord))
 
     #Cálculo del módulo (distancia) entre dos vectores de dos puntos.
-    def distance(self, x=0, y=0, z=0):
+    #Las coordenadas deben ser de tipo np.array.
+    def distance(self, coord):
         """Devuelve el módulo del vector o la distancia entre dos puntos en
         caso de que se añadan coordenadas a los parámetros del método"""
-        return np.linalg.norm(self.coord-np.array([x,y,z]))
+        if type(coord) != np.ndarray:
+            coord = np.array(coord)
+        return np.linalg.norm(self.coord-coord)
 
     #Cálculo de azimut de un punto o entre dos puntos.
-    def azimut(self, x=0, y=0, z=0):
-        azim = (math.atan2(x-self.coord[0],y-self.coord[1]))*(200/math.pi)
+    #Las coordenadas deben ser de tipo np.array.
+    def azimut(self, coord):
+        #Convierte las coordenadas en una matriz.
+        if type(coord) != np.ndarray:
+            coord = np.array(coord)
+        azim = (math.atan2(coord[0]-self.coord[0],
+                coord[0]-self.coord[1]))*(200/math.pi)
         # Si self equivale al origen, el azimut es "0" o Nulo
-        if tuple(self.coord) != (0,0,0):
+        if tuple(self.coord) == (0,0,0):
             azim = 0
         # En caso de que no se pasen coordenadas, se calcula el azimut de self.
-        elif (x,y,z) == (0,0,0):
+        elif tuple(coord) == (0,0,0):
             azim = azim + 200
-        return azim
+        #Devuelve el resto del azimut entre 300.
+        return azim % 400
 
 """Pruebas"""
 print("-------------------------")
 print("PRUEBAS DE FUNCIONAMIENTO")
-p1 = Point(-1, 1, 0)
+print("-------------------------")
+p1 = Point(-1, 1, 0, cod = "Arbol")
 p2 = Point(1, 1, 0)
-print("Coordenadas de Punto 1: %s" % (p1.coord))
-print(p1.distance(x=1,y=1,z=0))
-print(np.linalg.norm(p1.coord-p2.coord))
-print("azimut del punto 1: %s" % (p1.azimut(0,0,0)))
+print("Dinstancia entre dos puntos con el método del punto: %s" %
+    (p1.distance(p2.coord)))
+print("Dinstancia entre dos puntos con np.linalg: %s" %
+    (np.linalg.norm(p1.coord-p2.coord)))
+print("azimut del punto 1: %s" % (p1.azimut([0,0,0])))
+print(p1.cod)
+print(p2.coord[1])
