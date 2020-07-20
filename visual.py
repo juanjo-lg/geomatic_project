87edec6ff6d89960c94e83d6401c501f3affa351
juanjo-lg <juanjo_lg@hotmail.es>
2020-07-20 18:01:03 +0200

    Add visual.py

---
+++ visual.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 17:13:00 2020
+
+@author: Juanjo_LG
+"""
+
+import sys
+import tkinter as tk
+
+#Creación de una instancia de la ventana padre.
+root = tk.Tk()
+root.title("TFG - Juan José Lorenzo Gutiérrez")
+#Tamaño de la pantalla del dispositivo
+screen_size = root.winfo_screenwidth(),root.winfo_screenheight()
+#Ajuste de la geometría para que quede perfecta en el portatil.
+#El formato es: 'Ancho'x'Alto'+'X'+'Y'
+root.geometry("%dx%d+%d-%d" % (screen_size[0], screen_size[1]-80,-10,40))
+
+#Inicializando la raiz.
+root.mainloop()
