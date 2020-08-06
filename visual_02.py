5fbe740b33afdc2e1fac1458c16694467a1ce93a
juanjo-lg <juanjo_lg@hotmail.es>
2020-08-06 18:34:00 +0200

    Update visual_02.py

--- visual_02.py
+++ visual_02.py
@@ -17,13 +17,14 @@ class App(tk.Tk):
     def __init__(self):
         super(App, self).__init__()
         # Listas provisionales de apoyo.
+        self.splitters = ["espacio","tabulador","coma","punto y coma"]
         self.file_opened = []
         self.item_to_show = []
         self.config_root()
         self.config_menu()
         self.config_frames()
-        self.btn = tk.Button(text = "Cerrar", command = self.close)
-        self.btn.pack()
+        self.draw_canvas(self.fr_canvas)
+        self.draw_note(self.fr_note)
     def config_root(self):
         # Configuración de ventana principal.
         self.title("TFG - Juan José Lorenzo Gutiérrez")
@@ -53,25 +54,80 @@ class App(tk.Tk):
         self.config(menu=self.menubar)
     def config_frames(self):
         # Configuración de los Frames de la App.
-        self.fr_canvas = tk.Frame()
-        self.fr_table = tk.Frame(bg='gray38')
+        self.fr_canvas = tk.Frame(width=self.screen_size[0]/2)
         self.fr_note = tk.Frame()
+        self.fr_table = tk.Frame(bg='gray38',width=self.screen_size[0]/2)
         # Colocación de Frames.
         self.fr_canvas.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
-        self.fr_table.pack(side=tk.TOP,fill=tk.Y)
-        self.fr_note.pack(fill=tk.BOTH)
-        def draw_canvas():
-            # Configuración de canvas.
-            self.fig = Figure(figsize=(5,5), dpi=100)
-            self.ax = self.fig.add_subplot(1,1,1)
-            self.ax.set_xlabel('Coordenadas X')
-            self.ax.set_ylabel('Coordenadas Y')
-            self.canvas = FigureCanvasTkAgg(self.fig, master=self.fr_canvas)
-            self.canvas.draw()
-            self.canvas.get_tk_widget().pack(
-                side=tk.LEFT, fill=tk.BOTH, expand = 1)
-        # Inicialización de canvas.
-        draw_canvas()
+        self.fr_note.pack(side=tk.TOP,fill=tk.Y)
+        self.fr_table.pack(fill=tk.Y)
+    def draw_note(self,master):
+        # Configuracion del Notebook con estilos de diseño.
+        note_style = ttk.Style()
+        # De esta forma se quitan los bordes.
+        note_style.theme_use('default')
+        # 'TNotebook','TNotebook.Tab' y 'TFrame' son estilos de ttk.
+        note_style.configure('TNotebook',borderwidth=0)
+        # Quita las líneas punteadas de las tablas al seleccionarlas.
+        note_style.configure('TNotebook.Tab',borderwidth=0,
+            focuscolor=note_style.configure(".")["background"])
+        note_style.configure('TFrame',borderwidth=0,background="gray60")
+        # Con 'readonly' no salen las palabras enmarcadas.
+        note_style.configure('TCombobox',
+            selectbackground=[('readonly','white')],
+            selectforeground=[('readonly','black')],
+            fieldbackground=[('readonly','gray60')])
+        # Constructor del Notebook.
+        self.notebook = ttk.Notebook(master,style='TNotebook',
+            width=int(self.screen_size[0]/2),
+            height=int(self.screen_size[1]/12))
+        """print(self.screen_size[1]/12)"""
+        # Funciones para crear distintas pestañas.
+        def file():
+            self.fr_file = ttk.Frame(master,style='TFrame')
+            # Botón para abrir archivos.
+            self.btn_open = tk.Button(
+                self.fr_file,text='Abrir',
+                height=int(self.screen_size[1]/12),
+                width=10,bg="grey60",
+                relief=tk.FLAT)
+            self.btn_open.pack(side=tk.LEFT)
+            # Frame para separadores.
+            self.fr_separator = ttk.Frame(self.fr_file,style='TFrame')
+            self.fr_separator.pack(side=tk.LEFT)
+            # Etiqueta de separadores.
+            self.lbl_separator = tk.Label(self.fr_separator,
+                text='Separador de puntos',
+                background="gray60", fg="black")
+            self.lbl_separator.pack(side=tk.TOP,pady=5)
+            # Combobox con los distintos separadores.
+            self.cmb_separator = ttk.Combobox(self.fr_separator,values=self.splitters)
+            #self.cmb_separator["values"] = self.splitters
+            self.cmb_separator.pack(pady=5)
+            self.btn_close = tk.Button(
+                self.fr_file,text='Cerrar',
+                command=self.close,
+                height=int(self.screen_size[1]/12),
+                width=10,bg="grey60",
+                relief=tk.FLAT)
+            self.btn_close.pack(side=tk.LEFT)
+            self.notebook.add(self.fr_file,text="Archivo")
+        def calculus():
+            self.fr_calc = ttk.Frame(master,style='TFrame')
+            self.notebook.add(self.fr_calc,text="Cálculos")
+        # Inicialización de pestañas file y calculus
+        file()
+        calculus()
+        self.notebook.pack(fill=tk.BOTH,expand=True)
+    def draw_canvas(self,master):
+        # Configuración de canvas.
+        self.fig = Figure(figsize=(5,5), dpi=100)
+        self.ax = self.fig.add_subplot(1,1,1)
+        self.ax.set_xlabel('Coordenadas X')
+        self.ax.set_ylabel('Coordenadas Y')
+        self.canvas = FigureCanvasTkAgg(self.fig, master=master)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.TOP,fill=tk.BOTH,expand=True)
     def close(self, event=None):
         # Función para cerrar el programa.
         mess = "¿Está seguro de que quiere salir del programa?"
